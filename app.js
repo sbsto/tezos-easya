60047018c9b78616494aed6739374757ef8c1854
sbsto <37947148+sbsto@users.noreply.github.com>
2022-11-26 17:35:18 +0000

    add sign form

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
    res.redirect("/sign")
 });
 
-http.listen(3000, function() {
-   console.log('listening on *:3000')
+http.listen(3001, function() {
+   console.log('listening on *:3001')
 });
