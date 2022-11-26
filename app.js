6070e0a01746915558ef5f640687d5f78ff43ead
sbsto <37947148+sbsto@users.noreply.github.com>
2022-11-26 18:27:33 +0000

    add form post handling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 const http = require('http').Server(app);
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -10,6 +12,11 @@ app.use(express.static("public"))
 app.get('/', function(req, res) {
    res.redirect("/sign")
 });
+
+// receive form data
+app.post('/sign', function(req, res) {
+  console.log(req.body)
+})
 
 http.listen(3001, function() {
    console.log('listening on *:3001')
